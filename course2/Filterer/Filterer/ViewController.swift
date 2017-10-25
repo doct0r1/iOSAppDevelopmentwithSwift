b40f3ba45d2e995fb259043638646904f7aa95de
Mustafa Jamal <mustafa52333@gmail.com>
2017-10-25 11:39:10 -0700

    redOnlyNotFunctional
    needs to connect functions w/ buttons

---
+++ course2/Filterer/Filterer/ViewController.swift
@@ -0,0 +1,149 @@
+//
+//  ViewController.swift
+//  Filterer
+//
+//  Created by Mustafa Jamal on 2015-09-22.
+//  Copyright © 2017 Mustafa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var filteredImage: UIImage?
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var secondaryMenu: UIView!
+    @IBOutlet var bottomMenu: UIView!
+    
+    @IBOutlet var filterButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        secondaryMenu.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.3)
+        secondaryMenu.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    // MARK: Share
+    @IBAction func onShare(sender: AnyObject) {
+        let activityController = UIActivityViewController(activityItems: ["Check out our really cool app", imageView.image!], applicationActivities: nil)
+        presentViewController(activityController, animated: true, completion: nil)
+    }
+    
+    // MARK: New Photo
+    @IBAction func onNewPhoto(sender: AnyObject) {
+        let actionSheet = UIAlertController(title: "New Photo", message: nil, preferredStyle: .ActionSheet)
+        
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .Default, handler: { action in
+            self.showCamera()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Album", style: .Default, handler: { action in
+            self.showAlbum()
+        }))
+        
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        self.presentViewController(actionSheet, animated: true, completion: nil)
+    }
+    
+    func showCamera() {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .Camera
+        
+        presentViewController(cameraPicker, animated: true, completion: nil)
+    }
+    
+    func showAlbum() {
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .PhotoLibrary
+        
+        presentViewController(cameraPicker, animated: true, completion: nil)
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        dismissViewControllerAnimated(true, completion: nil)
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: Filter Menu
+    @IBAction func onFilter(sender: UIButton) {
+        if (sender.selected) {
+            hideSecondaryMenu()
+            sender.selected = false
+        } else {
+            showSecondaryMenu()
+            sender.selected = true
+        }
+    }
+    
+    func showSecondaryMenu() {
+        view.addSubview(secondaryMenu)
+        
+        let bottomConstraint = secondaryMenu.bottomAnchor.constraintEqualToAnchor(bottomMenu.topAnchor)
+        let leftConstraint = secondaryMenu.leftAnchor.constraintEqualToAnchor(view.leftAnchor)
+        let rightConstraint = secondaryMenu.rightAnchor.constraintEqualToAnchor(view.rightAnchor)
+        
+        let heightConstraint = secondaryMenu.heightAnchor.constraintEqualToConstant(44)
+        
+        NSLayoutConstraint.activateConstraints([bottomConstraint, leftConstraint, rightConstraint, heightConstraint])
+        
+        view.layoutIfNeeded()
+        
+        self.secondaryMenu.alpha = 0
+        UIView.animateWithDuration(0.4) {
+            self.secondaryMenu.alpha = 1.0
+        }
+    }
+
+    func hideSecondaryMenu() {
+        UIView.animateWithDuration(0.4, animations: {
+            self.secondaryMenu.alpha = 0
+            }) { completed in
+                if completed == true {
+                    self.secondaryMenu.removeFromSuperview()
+                }
+        }
+    }
+    
+    @IBAction func redFilter(sender: UIButton) {
+        if (sender.selected) {
+            UIImageView(image: onRed(filteredImage!))
+        }
+    }
+
+    func onRed(input: UIImage) -> UIImage {
+        let image = input
+        var rgbImage = RGBAImage.init(image: image)!
+        let avgRed = 107
+        
+        for y in 0..<rgbImage.height {
+            for x in 0..<rgbImage.width {
+                let index = y * rgbImage.width + x
+                var pixel = rgbImage.pixels[index]
+                let redDelta = Int(pixel.red) - avgRed
+                
+                var modifier = 1 + 4 * (Double(y) / Double(rgbImage.height))
+                if (Int(pixel.red) < avgRed) {
+                    modifier = 1
+                }
+                
+                pixel.red = UInt8(max(min(255, round(Double(avgRed)) + modifier * Double(redDelta)), 0))
+                rgbImage.pixels[index] = pixel
+            }
+        }
+        let final = rgbImage.toUIImage()
+        return final!
+    }
+
+}
